6fb25400f481f31cfb8c0a54f970fc31a5e3a4b8
kevin940726 <kevin830726@gmail.com>
2017-03-05 02:22:51 +0800

    ✨ Feat: add cli to auto generate new post template

---
+++ post.js
@@ -0,0 +1,46 @@
+const inquirer = require('inquirer');
+const dedent = require('dedent');
+const dateFormat = require('dateformat');
+const fs = require('fs');
+const sanitize = require('sanitize-filename');
+const chalk = require('chalk');
+
+function createTemplate({
+  title = '',
+  hero = '',
+} = {}) {
+  return dedent`
+    ---
+    title: '${title}'
+    date: ${dateFormat(new Date(), 'yyyy-mm-dd hh:MM:ss')}
+    layout: Post
+    hero: ${hero}
+    ---
+  `;
+}
+
+inquirer.prompt([
+  {
+    type: 'input',
+    name: 'title',
+    message: 'Enter the post title:',
+  },
+  {
+    type: 'input',
+    name: 'hero',
+    message: 'Enter the hero image url path (optional):',
+  },
+]).then(answers => {
+  const template = createTemplate(answers);
+
+  const filename = `${sanitize(answers.title)}.md`;
+
+  fs.writeFile(filename, template, 'utf8', (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    console.log(`${chalk.yellow(`🎉  Successfully create post ${chalk.bold(filename)} !`)}`);
+  })
+});
